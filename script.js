a51f26906c0cc87711c9b4717246ef193b7fe265
Mike Jackson <mike.jackson@mjconsult.co.uk>
2023-09-11 16:57:50 +0100

    instructions and answer boxes added

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const resultsBoxes = document.getElementById("resultsBoxes");
+const keyPressedInfo = document.getElementById("keyPressedInfo");
+const eventCodeInfo = document.getElementById("eventCodeInfo");
+const charCodeValue = document.getElementById("charCodeValue");
 
-
-
+document.addEventListener("keypress", (event) => {
+  console.log(event);
+  //   keyPressedInfo.textContent = `You pressed: ${event.key}`;
+  //   eventCodeInfo.textContent = `Event code: ${event.code}`;
+  //   charCodeValue.textContent = `Char Code: ${event.charCode}`;
+  keyPressedInfo.textContent = event.key;
+  eventCodeInfo.textContent = event.code;
+  charCodeValue.textContent = event.charCode;
+  resultsBoxes.style.display = "block";
+  resultsBoxes.style.display = "flex";
+  resultsBoxes.style.textAlign = "center";
+});
