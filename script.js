ac92833be4cfb55ead678df79f7e21ffd379db92
Mike Jackson <mike.jackson@mjconsult.co.uk>
2023-09-12 11:07:42 +0100

    clear screen button added

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ document.addEventListener("keypress", (event) => {
   instructions.textContent = `You pressed: ${event.key}`;
   //   instructions.style.color="red"
   eventCodeInfo.textContent = event.code;
-  eventCodeInfo.style.width="175px"
+  eventCodeInfo.style.width = "175px";
   keyPressedInfo.textContent = event.key;
-  keyPressedInfo.style.width="175px"
+  keyPressedInfo.style.width = "175px";
   charCodeValue.textContent = event.charCode;
-  charCodeValue.style.width="175px"
+  charCodeValue.style.width = "175px";
   resultsBoxes.style.display = "block";
   resultsBoxes.style.display = "flex";
   resultsBoxes.style.justifyContent = "space-evenly";
@@ -26,6 +26,15 @@ document.addEventListener("keypress", (event) => {
   eventCodeInfo.style.color = "red";
   keyPressedInfo.style.color = "red";
   charCodeValue.style.color = "red";
-  clearScreen.style.display="block"
-  clearScreen.style.margin="auto"
+  clearScreen.style.display = "block";
+  clearScreen.style.margin = "auto";
+});
+
+document.addEventListener("click", () => {
+  // Is there a way to set the textContent back to the original settng ?
+  // ie the default value
+  instructions.textContent =
+    "Press any key to get the Javascript key code info";
+  resultsBoxes.style.display = "none";
+  clearScreen.style.display = "none";
 });
