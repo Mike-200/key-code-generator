40b383a477b208fe1125d11aed5a816379adaf3e
Mike Jackson <mike.jackson@mjconsult.co.uk>
2023-09-12 11:01:41 +0100

    result boxes all made the same width

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
+const instructions = document.getElementById("instructions");
 const resultsBoxes = document.getElementById("resultsBoxes");
-const keyPressedInfo = document.getElementById("keyPressedInfo");
 const eventCodeInfo = document.getElementById("eventCodeInfo");
+const keyPressedInfo = document.getElementById("keyPressedInfo");
 const charCodeValue = document.getElementById("charCodeValue");
+const clearScreen = document.getElementById("clearScreen");
 
 document.addEventListener("keypress", (event) => {
-  console.log(event);
+  // console.log(event);
   //   keyPressedInfo.textContent = `You pressed: ${event.key}`;
   //   eventCodeInfo.textContent = `Event code: ${event.code}`;
   //   charCodeValue.textContent = `Char Code: ${event.charCode}`;
-  keyPressedInfo.textContent = event.key;
+  instructions.textContent = `You pressed: ${event.key}`;
+  //   instructions.style.color="red"
   eventCodeInfo.textContent = event.code;
+  eventCodeInfo.style.width="175px"
+  keyPressedInfo.textContent = event.key;
+  keyPressedInfo.style.width="175px"
   charCodeValue.textContent = event.charCode;
+  charCodeValue.style.width="175px"
   resultsBoxes.style.display = "block";
   resultsBoxes.style.display = "flex";
   resultsBoxes.style.justifyContent = "space-evenly";
   resultsBoxes.style.textAlign = "center";
   resultsBoxes.style.padding = "50px";
+  eventCodeInfo.style.color = "red";
+  keyPressedInfo.style.color = "red";
+  charCodeValue.style.color = "red";
+  clearScreen.style.display="block"
+  clearScreen.style.margin="auto"
 });
